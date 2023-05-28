b780403f45ed17f384ef869bb05d68906771804f
Vladislav <90531033+molch4nov@users.noreply.github.com>
2023-05-28 16:11:52 +0300

    ss

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const db = require('./db.js')
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+    db.authenticate()
+        .catch(error => console.error(error))
   })
